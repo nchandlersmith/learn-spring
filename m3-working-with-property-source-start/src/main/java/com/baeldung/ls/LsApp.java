0f1bff1b65c8e720f21810d955d62f3d959a953c
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2020-04-13 18:19:01 -0500

    adds additional properties

--- m3-working-with-property-source-start/src/main/java/com/baeldung/ls/LsApp.java
+++ m3-working-with-property-source-start/src/main/java/com/baeldung/ls/LsApp.java
@@ -1,10 +1,25 @@
 package com.baeldung.ls;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LsApp {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LsApp.class);
+
+    @Value("${additional.info}")
+    private String additional;
+
+    @PostConstruct
+    public void postConstruct() {
+        LOG.info("Additional property {}", additional);
+    }
 
     public static void main(final String... args) {
         SpringApplication.run(LsApp.class, args);
--- m3-working-with-property-source-start/src/main/java/com/baeldung/ls/config/AppConfig.java
+++ m3-working-with-property-source-start/src/main/java/com/baeldung/ls/config/AppConfig.java
@@ -1,7 +1,9 @@
 package com.baeldung.ls.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:additional.properties")
 @Configuration
 public class AppConfig {
 
