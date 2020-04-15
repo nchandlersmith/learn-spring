ef47a38ba0c646447d5713df3656503e81199656
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2020-04-15 16:21:39 -0500

    adds our own object mapper

--- m4-boot-auto-configuration-start/src/main/java/com/baeldung/ls/config/AppConfig.java
+++ m4-boot-auto-configuration-start/src/main/java/com/baeldung/ls/config/AppConfig.java
@@ -1,8 +1,15 @@
 package com.baeldung.ls.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
