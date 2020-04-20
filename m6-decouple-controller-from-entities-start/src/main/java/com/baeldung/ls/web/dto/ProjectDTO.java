7e6ca8fcc7013719943d54e2179bed25887f6f6c
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2020-04-20 15:35:51 -0500

    adds project dto

---
+++ m6-decouple-controller-from-entities-start/src/main/java/com/baeldung/ls/web/dto/ProjectDTO.java
@@ -0,0 +1,94 @@
+package com.baeldung.ls.web.dto;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import com.baeldung.ls.persistence.model.Task;
+
+public class ProjectDTO {
+
+    private Long id;
+    private String name;
+    private LocalDate dateCreated;
+    private Set<Task> tasks;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((dateCreated == null) ? 0 : dateCreated.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((tasks == null) ? 0 : tasks.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ProjectDTO other = (ProjectDTO) obj;
+        if (dateCreated == null) {
+            if (other.dateCreated != null)
+                return false;
+        } else if (!dateCreated.equals(other.dateCreated))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (tasks == null) {
+            if (other.tasks != null)
+                return false;
+        } else if (!tasks.equals(other.tasks))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ProjectDTO [dateCreated=" + dateCreated + ", id=" + id + ", name=" + name + ", tasks=" + tasks + "]";
+    }
+}
