6971de5d47961957a4b98cb29ac885816be2105d
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2020-04-20 15:03:07 -0500

    improves error handling; creates project

--- m6-expanding-our-first-controller-start/src/main/java/com/baeldung/ls/web/controller/ProjectController.java
+++ m6-expanding-our-first-controller-start/src/main/java/com/baeldung/ls/web/controller/ProjectController.java
@@ -1,20 +1,36 @@
 package com.baeldung.ls.web.controller;
 
-import java.time.LocalDate;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.baeldung.ls.persistence.model.Project;
+import com.baeldung.ls.service.IProjectService;
 
 @RestController
 @RequestMapping(value = "/projects")
 public class ProjectController {
 
-    @GetMapping(path = "/1")
-    public Project findOne() {
-        return new Project("testName", LocalDate.now());
+    @Autowired
+    IProjectService projectService;
+
+    public ProjectController(IProjectService projectService) {
+        this.projectService = projectService;
     }
 
+    @GetMapping(path = "/{id}")
+    public Project findOne(@PathVariable Long id) {
+        return this.projectService.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(path = "/create")
+    public void create(@RequestBody Project newProject) {
+        this.projectService.save(newProject);
+    }
 }
