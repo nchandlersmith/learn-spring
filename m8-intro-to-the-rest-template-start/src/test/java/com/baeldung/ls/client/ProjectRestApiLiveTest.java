27f72cc491fa2bf02648ca7a20d92c333bbfa50c
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2020-04-25 15:31:50 -0500

    refactor

--- m8-intro-to-the-rest-template-start/src/test/java/com/baeldung/ls/client/ProjectRestApiLiveTest.java
+++ m8-intro-to-the-rest-template-start/src/test/java/com/baeldung/ls/client/ProjectRestApiLiveTest.java
@@ -2,10 +2,11 @@ package com.baeldung.ls.client;
 
 import static org.junit.Assert.assertThat;
 
+import javax.annotation.PostConstruct;
+
 import com.baeldung.ls.web.dto.ProjectDto;
 
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -18,13 +19,17 @@ public class ProjectRestApiLiveTest {
 
     @LocalServerPort
     private int port;
-    private String BASE_URL;
+    private String baseUrl;
     private RestTemplate restTemplate = new RestTemplate();
+
+    @PostConstruct
+    public void setupPort() {
+        baseUrl = "http://localhost:" + port + "/projects/";
+    }
 
     @Test
     public void givenProjectExists_whenGet_thenSuccess() {
-        BASE_URL = "http://localhost:" + port + "/projects/";
-        ResponseEntity<ProjectDto> response = restTemplate.getForEntity(BASE_URL + "1", ProjectDto.class);
+        ResponseEntity<ProjectDto> response = restTemplate.getForEntity(baseUrl + "1", ProjectDto.class);
         assertThat(response.getStatusCodeValue(), Matchers.equalTo(200));
     }
 
